b06afa3ec7214a61451df2efe98e7692d8a42833
Morgan <mnordberg01@gmail.com>
2025-04-01 13:30:41 +0200

    fullscreen toggle?

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@
 #include "LoadTGA.h" 
 #include "VectorUtils4.h" 
 
-#define WINDOW_W 1280
-#define WINDOW_H 720
+#define WINDOW_W 720
+#define WINDOW_H 1280
+
 
 void init(void)
 {
@@ -22,6 +23,9 @@ void init(void)
 
 void display(void)
 {
+	if (glutKeyIsDown(GLUT_KEY_F7)) glutToggleFullScreen();
+
+	char glutKeyIsDown(unsigned char c);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glutSwapBuffers();
@@ -33,6 +37,7 @@ int main(int argc, char *argv[])
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitContextVersion(3, 2);
+	glutInitWindowPosition (10, 10);
 	glutInitWindowSize(WINDOW_W, WINDOW_H);
 	glutCreateWindow ("Martin doing the funny");
 	glutDisplayFunc(display); 
